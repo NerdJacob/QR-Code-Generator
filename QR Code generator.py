4c0cebc50bf4fc115b0ceefe7f419d5e1a1ec0ea
NerdJacob <Jacobespitz@gmail.com>
2024-03-23 08:09:58 -0400

    Update QR Code generator.py
    Fixed relative path

--- QR Code generator.py
+++ QR Code generator.py
@@ -1,3 +1,4 @@
+import os # os import needed for realtive file path
 try:
     import qrcode
 except:
@@ -8,18 +9,16 @@ except:
         subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
     install("qrcode")
-
-# ↓ Update with the directory path for the txt file with the list of codes 
-source_location = " "
-# ↓ Update with the directory path to place the QR files
-destination_location = " "
+# ↑ import qrcode (generator) or, on fail install and import
+dirname = os.path.dirname(__file__)
 
 list_of_codes = []
-file = open(source_location, 'r')
+inputfile = os.path.join(dirname, 'input.txt')
+file = open(inputfile, 'r')
 lines = file.readlines()
 file.close()
 
-for x in lines:
+for x in lines: # append each QR code from the input list to an array
     list_of_codes.append(x)
 
 # print(list_of_codes)
@@ -28,7 +27,8 @@ num = 1
 
 for x in range(len(list_of_codes)):
     img = qrcode.make(f'https://store.pokemongolive.com/offer-redemption?passcode={list_of_codes[x]}')
-    filename = f'{destination_location}{num}.png' 
+    filename = os.path.join(dirname, f'output/QR{num}.png')
+    # filename = f'output/QR{num}.png' 
     img.save(filename)
     type(img)
     num += 1
